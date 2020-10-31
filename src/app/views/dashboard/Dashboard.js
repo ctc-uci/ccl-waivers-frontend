ff9c802d0b22a37c5ee4edb15aecb364ee8ba4a9
Chris Tian <ctian1@users.noreply.github.com>
2020-10-31 16:54:04 -0700

    add router

--- src/app/views/dashboard/Dashboard.js
+++ src/app/views/dashboard/Dashboard.js
@@ -1,20 +1,92 @@
 import './Dashboard.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  // Route,
+  Link,
+  Route,
+} from 'react-router-dom';
+
+function Waivers() {
+  return (
+    <div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+      <div className="waiver-row">waiver #1</div>
+      <div className="waiver-row">waiver #2</div>
+      <div className="waiver-row">waiver #3</div>
+      <div className="waiver-row">waiver #4</div>
+      <div className="waiver-row">waiver #5</div>
+    </div>
+  );
+}
 
 function Dashboard() {
+  const routes = [
+    {
+      name: 'Waivers',
+      route: '/waivers',
+      component: Waivers,
+    },
+    {
+      name: 'Volunteers',
+      route: '/volunteers',
+      component: () => <p>volunteers</p>,
+    },
+    {
+      name: 'Forms',
+      route: '/forms',
+      component: () => <p>forms</p>,
+    },
+  ];
+
   return (
-    <div style={{ margin: '20px' }}>
-      <h1>Dashboard</h1>
-      <div>
-        <div className="dashboard-sidebar">
-          {/* <div>volunteers</div> */}
-          <div>waivers</div>
-        </div>
-        <div className="dashboard-content">
-          <div>
-            <div>waiver #1</div>
-            <div>waiver #2</div>
+    <div className="dashboard">
+      <div className="dashboard-header">
+        <div><h1>Dashboard</h1></div>
+      </div>
+      <div className="dashboard-container">
+        <Router>
+          <div className="dashboard-sidebar">
+            {routes.map((route) => (
+              <Link to={route.route}><div className="sidebar-link">{route.name}</div></Link>
+            ))}
           </div>
-        </div>
+          <div className="dashboard-content">
+            <Switch>
+              {routes.map((route) => (
+                <Route
+                  path={route.route}
+                >
+                  <route.component />
+                </Route>
+              ))}
+            </Switch>
+          </div>
+        </Router>
       </div>
     </div>
   );
