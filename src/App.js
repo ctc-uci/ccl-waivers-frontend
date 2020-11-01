22dd63693c6a5d7deacb6739e246d1345ad0f6c7
Jenny Lee <jennylee038@gmail.com>
2020-11-01 01:21:46 +0000

    added admin route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Admin from './Admin';
 import Waiver from './Waiver';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Route path="/" component={Waiver} />
+        <Route path="/admin" component={Admin} />
       </Router>
     </div>
   );
