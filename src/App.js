9fbbf9e7e82e283035f5fdba81816ebec65b5632
Ashley Liu <aslorange61@gmail.com>
2020-10-31 18:03:28 -0700

    Created the waiver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Waiver from './Waiver';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route />
+        <Route path="/" component={Waiver} />
       </Router>
     </div>
   );
