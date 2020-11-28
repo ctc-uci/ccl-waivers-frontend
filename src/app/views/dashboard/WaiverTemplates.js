f62c18bf9101e77593115fcbe9d68b84fc724d61
Chris Tian <ctian1@users.noreply.github.com>
2020-11-27 17:31:44 -0800

    start templates view

--- src/app/views/dashboard/WaiverTemplates.js
+++ src/app/views/dashboard/WaiverTemplates.js
@@ -1,9 +1,61 @@
-function WaiverTemplates() {
+import { Component } from 'react';
+import cloudUpload from '../../images/dashboard/cloud-upload.png';
+
+function WaiverTemplate() {
+  const templateStyle = {
+    height: '200px',
+    width: '154px',
+    border: '1px solid black',
+    marginLeft: '40px',
+    marginTop: '40px',
+  };
+
   return (
-    <div className="waiver-list">
-      <div>template</div>
+    <div style={templateStyle}>
+      template
     </div>
   );
+}
+
+class WaiverTemplates extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    const uploadStyle = {
+      height: '200px',
+      width: '154px',
+      border: '1px solid black',
+      marginLeft: '40px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+    };
+    const uploadImgStyle = {
+      margin: 'auto',
+      marginBottom: 0,
+      display: 'block',
+      width: '50px',
+    };
+
+    return (
+      <div style={{ display: 'flex', flexWrap: 'wrap' }} className="waiver-list">
+        <div style={uploadStyle}>
+          <img style={uploadImgStyle} src={cloudUpload} alt="upload to cloud" />
+          <div style={{ margin: 'auto', marginTop: '4px' }}>Upload template</div>
+        </div>
+        <WaiverTemplate />
+        <WaiverTemplate />
+        <WaiverTemplate />
+        <WaiverTemplate />
+        {/* <WaiverTemplate />
+        <WaiverTemplate />
+        <WaiverTemplate />
+        <WaiverTemplate /> */}
+      </div>
+    );
+  }
 }
 
 export default WaiverTemplates;
