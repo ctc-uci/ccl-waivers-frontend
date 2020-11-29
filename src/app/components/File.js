d6697f1912e7fd6158a3eccb198d77826ed20b41
Phillip Chan <chnphillip@gmail.com>
2020-11-29 00:26:03 -0800

    Added Files component

---
+++ src/app/components/File.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class File extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copiedLink: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      copiedLink: !(prevState.copiedLink),
+    }));
+  }
+
+  render() {
+    const { name } = this.props;
+    const { dateCreated } = this.props;
+    const { url } = this.props;
+    const { imagePreview } = this.props;
+    const isLinkClicked = this.state;
+
+    return (
+      <div>
+        <img src={imagePreview} alt="file preview" />
+        <h1>{name}</h1>
+        <text>
+          Created
+          {' '}
+          {dateCreated}
+        </text>
+        <br />
+        <a style={{ color: 'blue' }} href={url} onClick={this.handleClick}>{isLinkClicked.copiedLink ? '\u2713 Link Copied to Clipboard' : '\u26D3 Click Link' }</a>
+      </div>
+    );
+  }
+}
+
+File.propTypes = {
+  name: PropTypes.string.isRequired,
+  dateCreated: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  imagePreview: PropTypes.string.isRequired,
+};
+
+export default File;
--- src/app/views/dashboard/WaiverTemplates.js
+++ src/app/views/dashboard/WaiverTemplates.js
@@ -1,7 +1,15 @@
+import File from '../../components/File';
+
 function WaiverTemplates() {
+  function getDate(uploadDate) {
+    const date = `${uploadDate.toLocaleDateString(undefined, { month: 'short' })} ${uploadDate.toLocaleDateString(undefined, { day: '2-digit' })}, ${uploadDate.toLocaleDateString(undefined, { year: 'numeric' })}`;
+    return date;
+  }
+
   return (
     <div className="waiver-list">
       <div>template</div>
+      <File name="fileName" url="#" imagePreview="https://i.pinimg.com/originals/7f/d2/e4/7fd2e46b2da9819e667fb75caf475cf7.png" dateCreated={getDate(new Date())} />
     </div>
   );
 }
