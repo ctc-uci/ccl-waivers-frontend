7641047e43deb173caaac9fed44c55f4adb30d66
Xuan Vu <xuanbee@yahoo.com>
2021-02-09 03:52:29 -0800

    new upload template design

--- src/app/components/waiverTemplates/dropzone/DropZone.js
+++ src/app/components/waiverTemplates/dropzone/DropZone.js
@@ -56,8 +56,11 @@ function Dropzone({ onClose }) {
   const myUploadProgress = (myFileId) => (progressEvent) => {
     const { loaded, total } = progressEvent;
     const percent = Math.floor((loaded * 100) / total);
-    const bar = document.getElementById(myFileId).childNodes[2].firstChild.firstChild;
-    bar.style.width = `${percent}%`;
+    const progress = document.getElementById(myFileId);
+    progress.innerHTML = '<img alt="Progress spinner" src="/icons/spinner-icon.png" height="50px" className="spinning-progress" />';
+    if (percent === 100) {
+      progress.innerHTML = '<img alt="Progress spinner" src="/icons/check-icon.png" height="50px" />';
+    }
   };
 
   const uploadTemplates = () => {
@@ -77,7 +80,7 @@ function Dropzone({ onClose }) {
       if (uploaded) {
         setTimeout(() => {
           onClose();
-        }, 500);
+        }, 750);
       }
     }
   };
@@ -87,51 +90,41 @@ function Dropzone({ onClose }) {
     === event.target.parentNode.parentNode.parentNode.id);
     acceptedFiles.splice(index, 1);
   };
-
   return (
     <div className="container">
       <div className={dropzoneBox} {...getRootProps()}>
         <input {...getInputProps()} />
-        <span className="dropzone-firstLine">
-          Add files via Drag and Drop or
-          <button type="button" className="browse-btn" onClick={open}>
-            Browse Files
-          </button>
-        </span>
-
-        <div className="scrollable-div">
-          <table className="upload-table">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Size</th>
-                <th>Progress</th>
-              </tr>
-            </thead>
-            {acceptedFiles.map((file) => (
-              <tr id={file.path} key={file.path}>
-                <td>{file.path}</td>
-                <td>
-                  {file.size / 1000}
-                  {' '}
-                  KB
-                </td>
-                <td>
-                  <div className="progress-bar">
-                    <div className="progress" />
-                  </div>
-                </td>
-                <td>
-                  <button type="button" className="remove-file-btn" aria-label="Remove" onClick={deleteUploadedTemplate}><span aria-hidden="true">&times;</span></button>
-                </td>
-              </tr>
-            ))}
-          </table>
-        </div>
-
-        <button type="button" className="orange-btn template-upload-btn" onClick={uploadTemplates}>Upload All</button>
+        <span className="dropzone-text">Drag and drop a fillable PDF</span>
+        <span className="dropzone-text">or</span>
+        <button type="button" className="fileSelector" onClick={open}>
+          Select file from computer
+        </button>
       </div>
-
+      <aside>
+        <ul className="files-list">
+          {acceptedFiles.map((file) => (
+            <li key={file.path} className="file-item">
+              <span id={file.path}>
+                <div />
+              </span>
+              <p>
+                {file.path}
+                <br />
+                {file.size / 1000}
+                {' '}
+                KB
+              </p>
+              <button type="button" className="remove-file-btn" aria-label="Remove" onClick={deleteUploadedTemplate}>
+                <img alt="Close upload template form" src="/icons/close-icon.png" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <div className="popup-buttons">
+        <button type="button" className="orange-outline-btn popup-btn" onClick={onClose}>Cancel</button>
+        {acceptedFiles.length === 0 ? (<button type="button" className="disabled-btn orange-btn popup-btn" onClick={uploadTemplates}>Upload All</button>) : (<button type="button" className="orange-btn popup-btn" onClick={uploadTemplates}>Upload All</button>)}
+      </div>
     </div>
   );
 }
